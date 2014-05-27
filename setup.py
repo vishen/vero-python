3a97a16c8167678519e5904cd8812998c3b64cf0
vishen <pentecostjonathan@gmail.com>
2014-05-27 11:35:37 +1000

    Renamed test file

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ except ImportError:
 packages = [
     'vero'
 ]
-requires = ['requests==1.2.3']
-tests_require = ['mock==1.0.1']
+requires = ['requests==1.2.3', 'mock==1.0.1']
 
 setup(
     name='vero',
@@ -22,8 +21,6 @@ setup(
     include_package_data=True,
     zip_safe=True,
     install_requires=requires,
-    tests_require=tests_require,
-    test_suite='client_test',
     license=open('LICENSE.rst').read(),
     classifiers=(
         'Development Status :: 5 - Production/Stable',
